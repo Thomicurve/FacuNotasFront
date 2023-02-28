40b13a4e21ed6a6fb49ffe53b664a6d0fac81f77
thomicurve <thomigrax@gmail.com>
2023-02-27 23:29:53 -0300

    prettier and eslint configured / redux added

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
-import React from "react";
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import store from './store'
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-    , document.getElementById("app-main"))
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('app-main'),
+)
--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
 function Welcome() {
-  return (
-    <div>Bienvenido a FacuNotas!</div>
-  )
+  return <div>Bienvenido a FacuNotas!</div>
 }
 
 export default Welcome
